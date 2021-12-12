3ebfc85730ed392de537598b3444e35fa5287f05
chriskilinc <chriskilinc@gmail.com>
2021-12-12 17:50:55 +0100

    Omplement Application Insights

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './fonts/psyche.ttf'
+import { ApplicationInsights } from '@microsoft/applicationinsights-web'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -16,3 +17,12 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+const appInsights = new ApplicationInsights({
+  config: {
+    instrumentationKey: 'aa5bd5ff-f2ac-4f51-a199-56f050248d98'
+    /* ...Other Configuration Options... */
+  }
+});
+appInsights.loadAppInsights();
+appInsights.trackPageView(); // Manually call trackPageView to establish the current user/session/pageview
