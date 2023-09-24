d9546d06a37677edd906683ac613fd2edf10b994
Christopher Kilinc <chriskilinc@gmail.com>
2023-09-24 22:03:29 +0200

    change sleepy to sleepingcycles

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
             <h2>Projects</h2>
             <a className="text-pigments" target="blank" rel="noreferrer noopener" href="https://pigments.chriskilinc.com/">PRIGMENTS</a>
             <a className="text-scaletheory" target="blank" rel="noreferrer noopener" href="https://scaletheory.chriskilinc.com/">SCALETHEORY</a>
-            <a className="text-sleepy" target="blank" rel="noreferrer noopener" href="https://www.sleepingcycles.com/">sleepy</a>
+            <a className="text-sleepy" target="blank" rel="noreferrer noopener" href="https://www.sleepingcycles.com/">sleepingcycles</a>
           </section>
         </main>
       </div>
